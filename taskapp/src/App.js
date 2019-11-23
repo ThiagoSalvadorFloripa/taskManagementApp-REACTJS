99f8a6193a72b064053cf3c0413ff74c19847147
thiafo <thia>
2019-11-23 19:30:12 -0300

    Criação Page home e TaskResgister

--- taskapp/src/App.js
+++ taskapp/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import 'bootswatch/dist/materia/bootstrap.css'
 
-function App() {
-  return (
-    <div>
-      Vamoo
-    </div>
-  );
+import Home from './views/home'
+class App extends React.Component {
+  render(){
+    return(
+      <div>
+        <Home/>
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
---
+++ taskapp/src/views/taskRegister.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import 'bootswatch/dist/materia/bootstrap.css'
+
+class TaskResgister extends React.Component{
+    render(){
+        return(
+            <div>
+             
+            </div>
+        )
+    }
+}
+
+export default TaskResgister;

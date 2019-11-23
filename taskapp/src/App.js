65153425cceadff86bf3ca75ec746cf704de9378
thiafo <thia>
2019-11-23 20:56:32 -0300

    finalizando page taskRegister pegando os dados

--- taskapp/src/App.js
+++ taskapp/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import 'bootswatch/dist/materia/bootstrap.css'
 
+import TaskRegister from './views/taskRegister'
 import Home from './views/home'
 class App extends React.Component {
   render(){
     return(
       <div>
-        <Home/>
+        <TaskRegister/>
       </div>
     )
   }
--- taskapp/src/views/taskRegister.js
+++ taskapp/src/views/taskRegister.js
@@ -1,13 +1,53 @@
 import React from 'react';
 
 import 'bootswatch/dist/materia/bootstrap.css'
-
+import Card from '../componets/card'
+import FormGroup from '../componets/form-group'
 class TaskResgister extends React.Component{
+
+    state ={
+        title:'',
+        description:''
+    }
+
+    register = () => {
+        console.log(this.state)
+    }
+
     render(){
         return(
-            <div>
-             
+
+            <div className="container">
+                <Card title="Task Register">
+                <div className="row">
+                     <div className="col-lg-12">
+                         <div className="bs-component">
+                             <FormGroup label="Title: *" htmlFor="inputTitle">
+                                 <input type="text"
+                                        id="inputTitle"
+                                        className="form-control"
+                                        name="title"
+                                        onChange={e => this.setState({title: e.target.value})} />
+                             </FormGroup>
+
+                             <FormGroup label="Description: *" htmlFor="inputDescription">
+                                 <textarea type="text"
+                                        id="inputDescription"
+                                        className="form-control"
+                                        name="description"
+                                        onChange={e => this.setState({description: e.target.value})} />
+                             </FormGroup>
+
+                             <button onClick = {this.register} type="button" className="btn btn-primary">Save</button>
+                             <button type="button" className="btn btn-danger">Cancel</button>
+
+                         </div>
+                     </div>             
+                </div>
+
+                </Card>
             </div>
+           
         )
     }
 }

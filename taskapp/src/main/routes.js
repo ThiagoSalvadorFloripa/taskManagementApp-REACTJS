a844861082a0686b71dce8094bbe621725170b30
thiafo <thia>
2019-11-24 16:46:01 -0300

    criando pagina 404

--- taskapp/src/main/routes.js
+++ taskapp/src/main/routes.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import { Route, Switch, HashRouter } from 'react-router-dom'
+import { Route, Switch, HashRouter, Redirect} from 'react-router-dom'
 import Home from '../views/home'
 import TaskRegister from '../views/task/taskRegister'
+import NotFoundPage from '../views/notFoundPage'
 function Routes(){
     return(
         <HashRouter>
             <Switch>
+                <Route path="/" exact={true} component={Home} />
                 <Route path="/home" component={Home}/>
                 <Route path="/taskResgister" component={TaskRegister}/>
+                <Route path="*" component={NotFoundPage} />
+                <Route path="/404" component={NotFoundPage} />
+                    <Redirect to="/404" />
             </Switch>
         </HashRouter>
     )
